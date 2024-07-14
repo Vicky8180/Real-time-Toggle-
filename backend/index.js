d857192cb276d847b3dc1bb2216a9ded1b566e67
Vicky8180 <vyadav99x1@gmail.com>
2024-07-14 23:22:06 +0530

    deploy4

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     // origin: "http://localhost:3000",
-    origin:"https://toggleitnow.netlify.app/",
+    origin:"https://real-time-toggle.onrender.com",
     methods: ["GET", "PUT"]
   }
 });
